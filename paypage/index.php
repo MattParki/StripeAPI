3f053fb2b1512b46826b95c0860e5f502436b678
MattParki <mattparkinson_12@hotmail.com>
2021-04-12 14:19:33 +0100

    card will now submitted the form to the charge.php page

--- paypage/index.php
+++ paypage/index.php
@@ -10,10 +10,14 @@
 </head>
 <body>
 <div class="container">
+    <h2 class="my-4 text-center">Intro To React Course [£40]</h2>
     </div>
 <!-- the form that will be submitting to the charge.php page -->
     <form action="./charge.php" method="post" id="payment-form">
     <div class="form-row">
+    <input type="text" name="first_name" class="input form-control mb-3 StripeElement StripeElement--empty" placeholder="First Name">
+    <input type="text" name="last_name" class="input form-control mb-3 StripeElement StripeElement--empty" placeholder="Last Name">
+    <input type="email" name="email" class="input form-control mb-3 StripeElement StripeElement--empty" placeholder="Email Address">
         <div id="card-element" class="form-control">
         <!-- A Stripe Element will be inserted here. -->
         </div>
@@ -23,6 +27,9 @@
     <button>Submit Payment</button>
   </form>
 </div>
+
+<!-- JQuery and stripe CDN -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://js.stripe.com/v3/"></script>
 <script src="./js/charge.js"></script>
 
